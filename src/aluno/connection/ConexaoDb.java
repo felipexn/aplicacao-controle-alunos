adad73148818ad24a3be1a013f96754ccabbf32c
Luiz Felipe <108966595+felipexn@users.noreply.github.com>
2023-04-08 14:50:11 -0300

    Update ConexaoDb.java

--- src/aluno/connection/ConexaoDb.java
+++ src/aluno/connection/ConexaoDb.java
@@ -4,19 +4,18 @@ import java.sql.Connection;
 import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 import java.util.Scanner;
 
 public class ConexaoDb {
-
-    private static String USERNAME = "root";
+// colocando dados do banco de dados em variaveis.
+    private static String USERNAME = "root"; //put your root here
     private static String PASSWORD = "yourpassword"; // put your database root password here
     private static String DBURL = "jdbc:mysql://localhost:3306/alunos";
 
     public static Connection CreatConnection()
             throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-
+//declarando uma variavel do tipo connection e usando o metodo getConnection pra fazer a conexao com o banco de dados
         Connection con = DriverManager.getConnection(DBURL, USERNAME, PASSWORD);
 
         return con;
